ae197650f18fc64d0d6a6a8d5da738308f86df7f
abdo-soliman <abdomedono2014@gmail.com>
2017-12-15 20:35:55 +0200

    multi include fixed

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -12,8 +12,12 @@
     <link rel="stylesheet" href="css/animate.css">
     <link rel="stylesheet" href="css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/navbar.css" />
-    @if(isset($cssfile) && $cssfile !== "")
-        <link rel="stylesheet" href={{$cssfile}} />
+    @if(isset($cssfile))
+        @foreach($cssfile as $css)
+            @if($css !== "")
+                <link rel="stylesheet" href={{$css}} />
+            @endif
+        @endforeach
     @endif
     
     <title>@yield('title')</title>
@@ -65,8 +69,8 @@
                 </button>
                 <form class="log_form" action="#" method="post">
                     <h2>LOG IN</h2>
-                    <input id="Email" placeholder="USER NAME" name="login_username" type="text">
-                    <input id="password" placeholder="PASSWORD" name="login_password" type="password">
+                    <input id="login_username" placeholder="USER NAME" name="login_username" type="text">
+                    <input id="login_password" placeholder="PASSWORD" name="login_password" type="password">
                     <button type="submit" class="submit">LOGIN</button>
                 </form>
             </div>
@@ -84,10 +88,10 @@
                 </button>
                 <form class="log_form" action="#" method="post">
                     <h2>SIGN UP</h2>
-                    <input id="username" placeholder="USER NAME" name="username" type="text">
-                    <input id="Email" placeholder="EMAIL" name="signup_email" type="text">
-                    <input id="password" placeholder="PASSWORD" name="signup_password" type="password">
-                    <input id="password_confirm" placeholder="CONFIRM PASSWORD" name="signup_password_confirm" type="password">
+                    <input id="signup_username" placeholder="USER NAME" name="signup_username" type="text">
+                    <input id="signup_email" placeholder="EMAIL" name="signup_email" type="text">
+                    <input id="signup_password" placeholder="PASSWORD" name="signup_password" type="password">
+                    <input id="signup_password_confirm" placeholder="CONFIRM PASSWORD" name="signup_password_confirm" type="password">
                     <button type="submit" class="submit">SIGNUP</button>
                 </form>
             </div>
@@ -113,8 +117,12 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/navbar.js"></script>
-    @if(isset($jsfile) && $jsfile !== "")
-        <script src={{$jsfile}}></script>
+    @if(isset($jsfile))
+        @foreach($jsfile as $js)
+            @if($js !== "")
+                <script src={{$js}}></script>
+            @endif
+        @endforeach
     @endif
 </body>
 
