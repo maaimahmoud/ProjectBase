29b835809831de059b0664a7eae122380e3ac5f4
maaimahmoud <mai.kenawy97@eng-st.cu.edu.eg>
2017-12-15 19:59:40 +0200

    error

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,113 +5,55 @@
 @endsection
 
 @section('cssfile')
-    {{$cssfile = "" /*type your css file path between the double quotes note: all path are with respect to public folder (consider public folder as your root directory)*/}}
+    {{$cssfile = "css/style.css" /*type your css file path between the double quotes note: all path are with respect to public folder (consider public folder as your root directory)*/}}
 @endsection
 
 @section('jsfile')
-    {{$jsfile = "" /*type your js file path between the double quotes note: all path are with respect to public folder (consider public folder as your root directory)*/}}
+    {{$jsfile = "js/modernizr.custom.63321.js" /*type your js file path between the double quotes note: all path are with respect to public folder (consider public folder as your root directory)*/}}
 @endsection
 
 @section('content')
- 
-<!DOCTYPE html>
-<html>
-<head>
-<style> 
-.explore-area{
-        height: 610px;
-}
-.fixed-bg {
-    background-image:url("images/Slogan.png");
-    background-attachment: fixed;
-    background-position:absolute;
-    background-repeat: no-repeat;
-    background-size: cover;
-}
+	     		
+		<div class="container">	
+			<div class="main">
+				<div id="mi-slider" class="mi-slider">
+					<ul>
+						<li><a href="#"><img src="images/1.jpg" alt="img01"><h4>Boots</h4></a></li>
+						<li><a href="#"><img src="images/2.jpg" alt="img02"><h4>Oxfords</h4></a></li>
+						<li><a href="#"><img src="images/3.jpg" alt="img03"><h4>Loafers</h4></a></li>
+						<li><a href="#"><img src="images/4.jpg" alt="img04"><h4>Sneakers</h4></a></li>
+					</ul>
+					<ul>
+						<li><a href="#"><img src="images/5.jpg" alt="img05"><h4>Belts</h4></a></li>
+						<li><a href="#"><img src="images/6.jpg" alt="img06"><h4>Hats &amp; Caps</h4></a></li>
+						<li><a href="#"><img src="images/7.jpg" alt="img07"><h4>Sunglasses</h4></a></li>
+						<li><a href="#"><img src="images/8.jpg" alt="img08"><h4>Scarves</h4></a></li>
+					</ul>
+					<ul>
+						<li><a href="#"><img src="images/9.jpg" alt="img09"><h4>Casual</h4></a></li>
+						<li><a href="#"><img src="images/10.jpg" alt="img10"><h4>Luxury</h4></a></li>
+						<li><a href="#"><img src="images/11.jpg" alt="img11"><h4>Sport</h4></a></li>
+					</ul>
+					<ul>
+						<li><a href="#"><img src="images/12.jpg" alt="img12"><h4>Carry-Ons</h4></a></li>
+						<li><a href="#"><img src="images/13.jpg" alt="img13"><h4>Duffel Bags</h4></a></li>
+						<li><a href="#"><img src="images/14.jpg" alt="img14"><h4>Laptop Bags</h4></a></li>
+						<li><a href="#"><img src="images/15.jpg" alt="img15"><h4>Briefcases</h4></a></li>
+					</ul>
+					<nav>
+						<a href="#">Shoes</a>
+						<a href="#">Accessories</a>
+						<a href="#">Watches</a>
+						<a href="#">Bags</a>
+					</nav>
+				</div>
+			</div>
+		</div><!-- /container -->
+		<script type="text/javascript" src="js/jquery.catslider.js"></script>
+		<script>
+			$(function() {
 
-.walk-through{
-    height:610px;
-    background-color:#816447;
-    background-color: rgba(81,64,47,0.9);
-}
-.walk-through ul{
-    color: white;
-    font-family:Josefin-sans;
-    font-size: 45px;
-    padding-top: 100px;
-    /*padding-left: 100px;*/
-    list-style: none;
-}
-
-.explore-button{
-    margin-top: 465px;
-    margin-left: 47%;
-}
-.explore-area p{
-    color: white;  
-    margin-left :43%;
-}
-
-.mai{
-    position:absolute;
-}
-
-.btn:hover{
-    background-image:none;
-    background-color:#fcfd7b;
-    cursor: pointer;
-}
-
-</style>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script>
-    
-$(document).ready(function(){
-        $("li").mouseenter(function(){
-        $(this).css('text-decoration','underline');
-        $(this).css('cursor','pointer');
-            $(".mai").animate({"left": "-=500px"},"slow");
-    });
-        $("li").mouseleave(function(){
-        $(this).css('text-decoration','none');
-    });
-
-        $("#explore").on("click", function() {
-        var object = $(this);
-        $("html ,body").animate({
-            scrollTop: $("#" + object.data("scroll") + "-section").offset().top - 55}, 700);
-    });
-
-
-
-});
-
-</script>
-</head>
-
-<body>
- 
-<div class="fixed-bg">
-    <div class="explore-area">
-        
-    <button id="explore" type="button" class="explore-button btn btn-default btn-lg" data-scroll="walk-through">EXPLORE</button>
-        <p>If you have an account You can<br> <a href="#">Sign in</a> or <a href="#">Create a new account</a></p>
-    </div>
-    <div class="walk-through" id="walk-through-section">
-        <div class="mai">
-          <ul id="walk-through-list">
-            <li>First Year</li>
-            <li>Second Year</li>
-            <li>Third Year</li>
-            <li>Fourth Year</li>
-        </ul> 
-        </div>
-
-    </div>
-</div>
-
-</body>
-</html>
-
-    
+				$( '#mi-slider' ).catslider();
+			});
+		</script>  
 @endsection
